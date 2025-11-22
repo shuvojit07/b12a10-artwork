0f424001ad9ff1961157c4851c9a54f7a41b4467
Shuvojit Tikader <shuvojittikader@gmail.com>
2025-11-23 01:31:31 +0600

    6th commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
-// tailwind.config.js (ESM)
-import daisyui from 'daisyui';
+import daisyui from "daisyui";
 
 export default {
-  darkMode: ['class', '[data-theme="dark"]'],
+  darkMode: ["class", '[data-theme="dark"]'],
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: 'var(--primary)',
-        bg: 'var(--bg)',
-        card: 'var(--card)',
-        bodytext: 'var(--text)',
+        primary: "var(--primary)",
+        bg: "var(--bg)",
+        card: "var(--card)",
+        bodytext: "var(--text)",
       },
     },
   },
